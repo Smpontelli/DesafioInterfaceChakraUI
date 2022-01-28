fb486aa619df84c26f9834d10b2f0104566eb25e
Smpontelli <stefanopontelli@hotmail.com>
2022-01-27 22:04:32 -0300

    Challange finished

--- src/components/Continents/Cities100More.tsx
+++ src/components/Continents/Cities100More.tsx
@@ -9,22 +9,28 @@ export default function Cities100More(continent) {
     return (
         <Box p='4'>
 
-            <Flex gap='4' flexDir={[ "column", "column", "row"]} maxW={'1240px'} mx='auto' justify={'space-between'} mt='10' >
-                {continent100MoreCities[continent.continent.slug].map((city, index) => (
-                    <Box key={index}>
-                        <Image src={city.banner} alt={city.name}/>
-                        <Flex>
+            <Flex gap='4' flexDir={'column'}  maxW={'1240px'} mx='auto' justify={'space-between'} mt='10' >
+                <Text as="h4" fontFamily={'Poppins'} fontSize={'24'} fontWeight={'400'} color={'#47585B'}>Cidades +100</Text>
 
-                            <Box>
-                                <Text as='h4'>{city.name}</Text>
-                                <Text as='p'>{city.country}</Text>
+                <Flex flexWrap={'wrap'} flexDir={[ "column", "column", "row"]} gap="12px">
 
-                            </Box>
-                            <Image src={city.flag} alt={city.name}/>
-                        </Flex>
-                    </Box>
-                    
-                ))}
+                    {continent100MoreCities[continent.continent.slug].map((city, index) => (
+                        <Box key={index} maxW={'256px'} maxH={'279px'} mx='auto' my="5">
+                            <Image w={'100%'} src={city.banner} alt={city.name} borderTopRadius={'0.5rem'} objectFit='cover'/>
+                            <Flex w={'100%'} justify={'space-between'} p='24px' border={'1px'} borderTop={'0'} borderBottomRadius={'0.5rem'} borderColor={'yellow.500'} alignItems={'center'}>
+
+                                <Flex flexDir={'column'} gap='12px' >
+                                    <Text as='p' fontFamily={'Poppins'} fontSize={'20'} fontWeight={'500'} color={'#47585B'}>{city.name}</Text>
+                                    <Text as='p' fontFamily={'Poppins'} fontSize={'16'} fontWeight={'400'} color={'#999999'}>{city.country}</Text>
+
+                                </Flex>
+                                <Image src={city.flag} alt={city.name} boxSize='30px' objectFit='cover' borderRadius='full' border={'1px'}/>
+                            </Flex>
+                        </Box>
+                        
+                    ))}
+                </Flex>
+                
             </Flex>
         </Box>
 
